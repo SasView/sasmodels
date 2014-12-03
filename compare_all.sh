8fc26f8f033d626e159cf27a2aa6ca9b44fa1e86
Paul Kienzle <pkienzle@nist.gov>
2014-12-03 11:48:24 -0500

    fix batch comparison driver

--- compare_all.sh
+++ compare_all.sh
@@ -4,9 +4,11 @@ PYOPENCL_CTX=1
 PYTHONPATH=../bumps:../periodictable:../sasview/build/lib.macosx-10.5-x86_64-2.7
 export PYOPENCL_CTX PYTHONPATH
 
+set -x
+
 for dim in 1d100 2d32; do
-    ./compare_many.py all 100 $dim 0 > $dim_poly_0.csv
-    ./compare_many.py all 100 $dim 1e-5 > $dim_poly_1e-5.csv
-    ./compare_many.py all 100 $dim 1e-3 > $dim_poly_1e-3.csv
-    ./compare_many.py all 10000 $dim mono > $dim_mono.csv
+    ./compare_many.py all 100 $dim 0 > ${dim}_poly_0.csv
+    ./compare_many.py all 100 $dim 1e-5 > ${dim}_poly_1e-5.csv
+    ./compare_many.py all 100 $dim 1e-3 > ${dim}_poly_1e-3.csv
+    ./compare_many.py all 10000 $dim mono > ${dim}_mono.csv
 done
