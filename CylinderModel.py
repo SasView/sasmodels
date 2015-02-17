a1542aaea8c3c4b7e87ee6444cd821536e45ba02
Andrew Jackson <andrew.jackson@esss.se>
2015-02-17 09:22:36 +0100

    Removing cylinder_clone

--- CylinderModel.py
+++ CylinderModel.py
@@ -4,5 +4,5 @@ Drop-in replacement for sasview cylinder model.
 No rescaling or renaming of the parameters.
 """
 from sasmodels.sasview_model import make_class
-from sasmodels.models import cylinder_clone as cylinder
+from sasmodels.models import cylinder
 CylinderModel = make_class(cylinder, dtype='single')
