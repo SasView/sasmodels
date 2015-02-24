43bdddcd8c2b2ad9795e6bc69c43314fc3a617fd
Andrew Jackson <andrew.jackson@esss.se>
2015-02-24 20:21:11 +0100

    seemingly reasonable demo parameters for cylinder model that cause failure

--- sasmodels/models/cylinder.py
+++ sasmodels/models/cylinder.py
@@ -156,12 +156,12 @@ demo = dict(
     scale=1, background=0,
     sld=6, solvent_sld=1,
     #radius=5, length=20,
-    radius=260, length=290,
-    theta=30, phi=0,
+    radius=20, length=300,
+    theta=60, phi=60,
     radius_pd=.2, radius_pd_n=9,
     length_pd=.2,length_pd_n=10,
-    theta_pd=15, theta_pd_n=45,
-    phi_pd=15, phi_pd_n=1,
+    theta_pd=10, theta_pd_n=5,
+    phi_pd=10, phi_pd_n=5,
     )
 
 # For testing against the old sasview models, include the converted parameter
