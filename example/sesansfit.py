0a33675f5dfe63dcfc7345913a0bb44858641acc
Paul Kienzle <pkienzle@nist.gov>
2015-03-23 15:49:13 -0400

    update sesans example for refactored sasmodels.core

--- example/sesansfit.py
+++ example/sesansfit.py
@@ -1,16 +1,20 @@
 import numpy as np
 from bumps.names import *
 
-from sasmodels import bumps_model as sas
-kernel = sas.load_model('sphere', dtype='single')
-#kernel = sas.load_model('triaxial_ellipsoid', dtype='single')
+from sasmodels import core, bumps_model
+
+kernel = core.load_model("sphere", dtype='single')
+
 
 
 if True: # fix when data loader exists
 #    from sas.dataloader.readers\
     from sas.dataloader.loader import Loader
     loader=Loader()
-    data=loader.load('testsasview1.ses')
+    filename = 'testsasview1.ses'
+    data=loader.load(filename)
+    if data is None: raise IOError("Could not load file %r"%(filename,))
+    print dir(data), type(data)
     data.x /=10
     
 #    data = load_sesans('mydatfile.pz')
@@ -40,7 +44,7 @@ data.Rmax = 3*radius # [A]
 ##  Sphere parameters
 
 phi = Parameter(0.1, name="phi")
-model = sas.BumpsModel(data, kernel,
+model = bumps_model.BumpsModel(data, kernel,
     scale=phi*(1-phi), sld=7.0, solvent_sld=1.0, radius=radius)
 phi.range(0.001,0.90)
 #model.radius.pmp(40)
@@ -51,7 +55,7 @@ model.radius.range(100,10000)
 #model.radius_pd_n=0
 
 if False: # have sans data
-    sansmodel = sas.BumpsModel(sans_data, kernel, **model.parameters())
+    sansmodel = bumps_model.BumpsModel(sans_data, kernel, **model.parameters())
     problem = FitProblem([model, sansmodel])
 else:
     problem = FitProblem(model)
