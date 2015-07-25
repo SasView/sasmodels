f224873e62472b942530cb10cfcab1f4e13ebb87
paul butler <butlerpd@udel.edu>
2015-07-25 19:29:44 -0400

    updated documentation - see ticket #400

--- sasmodels/models/dab.py
+++ sasmodels/models/dab.py
@@ -5,28 +5,52 @@ Scattering model class for the DAB (Debye Anderson Brumberger) Model
 Definition
 ----------
 
-Calculates the scattering from a randomly distributed, two-phase system based on the Debye-Anderson-Brumberger (DAB)
-model for such systems. The two-phase system is characterized by a single length scale, the correlation length, which
-is a measure of the average spacing between regions of phase 1 and phase 2. **The model also assumes smooth interfaces**
-**between the phases** and hence exhibits Porod behavior (I ~ *q*\ :sup:`-4`) at large *q* (*QL* >> 1).
+Calculates the scattering from a randomly distributed, two-phase system based on
+the Debye-Anderson-Brumberger (DAB) model for such systems. The two-phase system
+is characterized by a single length scale, the correlation length, which is a
+measure of the average spacing between regions of phase 1 and phase 2. **The
+model also assumes smooth interfaces between the phases** and hence exhibits
+Porod behavior (I ~ *q*\ :sup:`-4`) at large *q* (*QL* >> 1).
 
 The DAB model is ostensibly a development of the earlier Debye-Bueche model.
 
-.. figure:: img/dab_1d.jpg
+*Definition*
 
-.. math:: I(q) = \text{scale}\cdot\frac{L^3}{(1 + (q L)^2)^2} + \text{background}
+.. math:: I(q) = \text{scale}\cdot\frac{L^3}{(1 + (q \cdot L)^2)^2} + \text{background}
 
 where scale is
 
 .. math:: \text{scale} = 8 \pi \phi (1-\phi)(\Delta \rho)^2
 
+and the parameter *L* is the correlation length.
+
+For 2D data: The 2D scattering intensity is calculated in the same way as 1D,
+where the *q* vector is defined as
+
+.. math:: q = \sqrt{q_x^2 + q_y^2}
+
+==============  ========  =============
+Parameter name  Units     Default value
+==============  ========  =============
+scale           None      1.0
+corr length L   |Ang|     50.0
+background      |cm^-1|   0.0
+==============  ========  =============
+
+
+.. figure:: img/dab_1d.jpg
+
+   1D plot using the default values (w/200 data point).
+
+
 Reference
 ---------
 
-P Debye, H R Anderson, H Brumberger, *Scattering by an Inhomogeneous Solid. II. The Correlation Function*
-*and its Application*, *J. Appl. Phys.*, 28(6) (1957) 679
+P Debye, H R Anderson, H Brumberger, *Scattering by an Inhomogeneous Solid. II.
+The Correlation Function and its Application*, *J. Appl. Phys.*, 28(6) (1957) 679
 
-P Debye, A M Bueche, *Scattering by an Inhomogeneous Solid*, *J. Appl. Phys.*, 20 (1949) 518
+P Debye, A M Bueche, *Scattering by an Inhomogeneous Solid*, *J. Appl. Phys.*,
+20 (1949) 518
 
 *2013/09/09 - Description reviewed by King, S and Parker, P.*
 
@@ -38,7 +62,7 @@ name = "dab"
 title = "DAB (Debye Anderson Brumberger) Model"
 description = """\
 
-F(q)= L^3/(1 + (q*L)^2)^2
+F(q)= scale * L^3/(1 + (q*L)^2)^2
 
 L: the correlation length
 
