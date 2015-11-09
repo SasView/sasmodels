9d76d29094c010883c753fea18b41a6c61e5561a
Paul Kienzle <pkienzle@nist.gov>
2015-11-09 15:43:00 -0500

    fix up docs to use latex

--- sasmodels/models/gaussian_peak.py
+++ sasmodels/models/gaussian_peak.py
@@ -1,20 +1,31 @@
 r"""
+
+Definition
+----------
+
 This model describes a Gaussian shaped peak on a flat background
 
-.. image:: img/image198.PNG
+.. math::
 
-with the peak having height of *I0* centered at *q0* and having a standard deviation of *B*. The FWHM (full-width
-half-maximum) is 2.354 B.
+    I(q) = (\text{scale}) \exp\left[ -\tfrac12 (q-q_0)^2 / \sigma^2 \right]
+        + \text{background}
 
-For 2D data: The 2D scattering intensity is calculated in the same way as 1D, where the *q* vector is defined as
+with the peak having height of *scale* centered at $q_0$ and having a standard
+deviation of $\sigma$. The FWHM (full-width half-maximum) is $2.354 \sigma$.
 
-.. image:: img/image040.gif
+For 2D data, scattering intensity is calculated in the same way as 1D,
+where the $q$ vector is defined as
 
-.. image:: img/image199.jpg
+.. math::
 
-*Figure. 1D plot using the default values (w/500 data points).*
+    q = \sqrt{q_x^2 + q_y^2}
 
-REFERENCE
+
+.. image:: img/gaussian_peak_1d.jpg
+
+    1D plot using the default values (w/500 data points).
+
+Reference
 ---------
 
 None.
@@ -26,13 +37,13 @@ name = "gaussian_peak"
 title = "Gaussian shaped peak"
 description = """
     Model describes a Gaussian shaped peak including a flat background
-    Provide F(q) = scale*exp( -1/2 *[(q-q0)/B]^2 )+ background
+    Provide F(q) = scale*exp( -1/2 *[(q-q0)/sigma]^2 )+ background
 """
 category = "shape-independent"
 
 #             ["name", "units", default, [lower, upper], "type","description"],
 parameters = [["q0", "1/Ang", 0.05, [-inf, inf], "", "Peak position"],
-              ["sigma", "1/Ang", 0.005, [-inf, inf], "",
+              ["sigma", "1/Ang", 0.005, [0, inf], "",
                "Peak width (standard deviation)"],
              ]
 
