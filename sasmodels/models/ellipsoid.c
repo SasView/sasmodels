3f832f95a1224aa53015664f7faacbe6d8a3ce2d
Paul Kienzle <pkienzle@nist.gov>
2016-01-07 13:46:36 -0500

    code cleanup

--- sasmodels/models/ellipsoid.c
+++ sasmodels/models/ellipsoid.c
@@ -3,13 +3,13 @@ double Iq(double q, double sld, double solvent_sld, double rpolar, double requat
 double Iqxy(double qx, double qy, double sld, double solvent_sld,
     double rpolar, double requatorial, double theta, double phi);
 
-double _ellipsoid_kernel(double q, double rpolar, double requatorial, double cos_alpha);
-double _ellipsoid_kernel(double q, double rpolar, double requatorial, double cos_alpha)
+double _ellipsoid_kernel(double q, double rpolar, double requatorial, double sin_alpha);
+double _ellipsoid_kernel(double q, double rpolar, double requatorial, double sin_alpha)
 {
     double sn, cn;
     double ratio = rpolar/requatorial;
     const double u = q*requatorial*sqrt(1.0
-                   + cos_alpha*cos_alpha*(ratio*ratio - 1.0));
+                   + sin_alpha*sin_alpha*(ratio*ratio - 1.0));
     SINCOS(u, sn, cn);
     //const double f = ( u==0.0 ? 1.0 : 3.0*(sn-u*cn)/(u*u*u) );
     const double usq = u*u;
@@ -34,9 +34,9 @@ double Iq(double q,
     //const double upper = 1.0;
     double total = 0.0;
     for (int i=0;i<76;i++) {
-        //const double cos_alpha = (Gauss76Z[i]*(upper-lower) + upper + lower)/2;
-        const double cos_alpha = 0.5*(Gauss76Z[i] + 1.0);
-        total += Gauss76Wt[i] * _ellipsoid_kernel(q, rpolar, requatorial, cos_alpha);
+        //const double sin_alpha = (Gauss76Z[i]*(upper-lower) + upper + lower)/2;
+        const double sin_alpha = 0.5*(Gauss76Z[i] + 1.0);
+        total += Gauss76Wt[i] * _ellipsoid_kernel(q, rpolar, requatorial, sin_alpha);
     }
     //const double form = (upper-lower)/2*total;
     const double form = 0.5*total;
@@ -56,6 +56,7 @@ double Iqxy(double qx, double qy,
 
     const double q = sqrt(qx*qx + qy*qy);
     SINCOS(theta*M_PI_180, sn, cn);
+    // TODO: check if this is actually sin(alpha), not cos(alpha)
     const double cos_alpha = cn*cos(phi*M_PI_180)*(qx/q) + sn*(qy/q);
     const double form = _ellipsoid_kernel(q, rpolar, requatorial, cos_alpha);
     const double s = (sld - solvent_sld) * form_volume(rpolar, requatorial);
