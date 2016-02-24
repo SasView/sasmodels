0471c7240e2e8d3abf5bf2e97c87ba95c44a5f34
Paul Kienzle <pkienzle@nist.gov>
2016-02-24 17:01:27 -0500

    minor formatting

--- sasmodels/models/multi_shell.c
+++ sasmodels/models/multi_shell.c
@@ -1,19 +1,3 @@
-double Iq(double q,
-          double core_radius,
-          double s_thickness,
-          double w_thickness,
-          double core_sld,
-          double shell_sld,
-          double n_pairs);
-
-double Iqxy(double qx, double qy,
-          double core_radius,
-          double s_thickness,
-          double w_thickness,
-          double core_sld,
-          double shell_sld,
-          double n_pairs);
-
 static
 double multi_shell_kernel(double q,
           double core_radius,
@@ -23,7 +7,7 @@ double multi_shell_kernel(double q,
           double shell_sld,
           double n_pairs)
 {
-	//calculate with a loop, two shells at a time
+    //calculate with a loop, two shells at a time
     int ii=0;
     double fval=0.0;
     double voli = 0.0;
@@ -45,14 +29,15 @@ double multi_shell_kernel(double q,
         //do 2 layers at a time
         ii+=1;
 
-	} while(ii<=n_pairs-1);  //change to make 0 < n_pairs < 2 correspond to
-	                         //unilamellar vesicles (C. Glinka, 11/24/03)
+    } while(ii<=n_pairs-1);  //change to make 0 < n_pairs < 2 correspond to
+                             //unilamellar vesicles (C. Glinka, 11/24/03)
 
     fval *= 1.0e-4*fval/voli;
 
     return(fval);
 }
 
+static
 double Iq(double q,
           double core_radius,
           double s_thickness,
@@ -70,6 +55,7 @@ double Iq(double q,
            n_pairs);
 }
 
+static
 double Iqxy(double qx, double qy,
           double core_radius,
           double s_thickness,
@@ -88,4 +74,3 @@ double Iqxy(double qx, double qy,
            shell_sld,
            n_pairs);
 }
-
