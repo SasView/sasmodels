15bd6e727112c9ec7422c00fd163671d9d457c21
Andrew Jackson <andrew.jackson@esss.se>
2016-03-17 14:38:20 +0100

    reverting to @smk98 updates to gaussian coil models from #246517d47

--- sasmodels/models/mono_gauss_coil.py
+++ sasmodels/models/mono_gauss_coil.py
@@ -13,19 +13,19 @@ Definition
 ----------
 
      *I(q)* = *scale* |cdot| *I* \ :sub:`0` |cdot| *P(q)* + *background*
-	 
+
 where
 
      *I*\ :sub:`0` = |phi|\ :sub:`poly` |cdot| *V* |cdot| (|rho|\ :sub:`poly` - |rho|\ :sub:`solv`)\  :sup:`2`
 
      *P(q)* = 2 [exp(-Z) + Z - 1] / Z \ :sup:`2`
-	 
-	 *Z* = (*q R* \ :sub:`g`)\ :sup:`2`
+
+     *Z* = (*q R* \ :sub:`g`)\ :sup:`2`
 
 and
 
-	 *V* = *M* / (*N*\ :sub:`A` |delta|)
-	 
+     *V* = *M* / (*N*\ :sub:`A` |delta|)
+
 Here, |phi|\ :sub:`poly` is the volume fraction of polymer, *V* is the volume of a polymer coil, *M* is the molecular weight of the polymer, *N*\ :sub:`A` is Avogadro's Number, |delta| is the bulk density of the polymer, |rho|\ :sub:`poly` is the sld of the polymer, |rho|\ :sub:`solv` is the sld of the solvent, and *R*\ :sub:`g` is the radius of gyration of the polymer coil.
 
 The 2D scattering intensity is calculated in the same way as the 1D, but where the *q* vector is redefined as
@@ -51,20 +51,20 @@ title =  "Scattering from monodisperse polymer coils"
 
 description =  """
     Evaluates the scattering from 
-	monodisperse polymer chains.
+    monodisperse polymer chains.
     """
 category =  "shape-independent"
 
 #             ["name", "units", default, [lower, upper], "type", "description"],
-parameters =  [["i_zero", "1/cm", 1.0, [-inf, inf], "", "Intensity at q=0"],
-               ["radius_gyration", "Ang", 50.0, [0.0, inf], "", "Radius of gyration"]]
+parameters =  [["i_zero", "1/cm", 70.0, [0.0, inf], "", "Intensity at q=0"],
+               ["radius_gyration", "Ang", 75.0, [0.0, inf], "", "Radius of gyration"]]
 
 # NB: Scale and Background are implicit parameters on every model
 def Iq(q, i_zero, radius_gyration):
     # pylint: disable = missing-docstring
-    z = (q * radius_gyration) ** 2
-    if q == 0:
-       inten = 1.0
+    z = (q * radius_gyration) * (q * radius_gyration)
+    if (q == 0).any():
+       inten = i_zero
     else:
        inten = i_zero * 2.0 * (exp(-z) + z - 1.0 ) / (z * z)
     return inten
@@ -76,8 +76,8 @@ def Iqxy(qx, qy, *args):
 #Iqxy.vectorized = True # Iqxy accepts an array of qx, qy values
 
 demo =  dict(scale = 1.0,
-            i_zero = 1.0,
-            radius_gyration = 50.0,
+            i_zero = 70.0,
+            radius_gyration = 75.0,
             background = 0.0)
 
 oldname =  "DebyeModel"
@@ -86,6 +86,6 @@ oldpars =  dict(scale = 'scale',
                background = 'background')
 
 tests =  [
-    [{'scale': 1.0, 'radius_gyration': 50.0, 'background': 0.0},
-     [0.0106939, 0.469418], [0.911141, 0.00362394]],
+    [{'scale': 70.0, 'radius_gyration': 75.0, 'background': 0.0},
+     [0.0106939, 0.469418], [57.1241, 0.112859]],
     ]
