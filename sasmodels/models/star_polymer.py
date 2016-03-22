6b4f7f6dcc3e41a1612f266b8d5ed21dd3a495d8
RichardHeenan <richard.heenan@stfc.ac.uk>
2016-03-22 17:51:26 +0000

    renamed variables, sorted issues in doc

--- sasmodels/models/star_polymer.py
+++ sasmodels/models/star_polymer.py
@@ -1,6 +1,6 @@
 r"""
-Calculates the scattering from fractal-like aggregates based on
-the Mildner reference. This model is also known as the Benoit Star model.
+The Benoit model for a simple star polymer, with Gaussian coils arms from 
+a common point.
 
 Definition
 ----------
@@ -24,11 +24,15 @@ and
 
     u = \left\langle R_{g}^2\right\rangle q^2
 
-is the square of the ensemble average radius-of-gyration of an arm.
+contains the square of the ensemble average radius-of-gyration of an arm.  Note that 
+when there is only one arm, $f$ = 1, the Debye Gaussian coil equation is recovered.
+Star polymers in solutions tend to have strong interparticle and osmotic effects, so 
+the Benoit equation may not work well. At small q the Guinier term and hence I(q=0)
+is the same as for $f$ arms of radius of gyration $R_g$, as described for the :ref:`mono_gauss_coil <mono-gauss-coil>`.
 
 
-Reference
----------
+References
+----------
 
 H Benoit *J. Polymer Science*, 11, 596-599 (1953)
 
@@ -40,20 +44,20 @@ from numpy import inf
 name = "star_polymer"
 title = "Star polymer model with Gaussian statistics"
 description = """
-        Scattering model class for 'Star polymer with Gaussian statistics'
+        Benoit 'Star polymer with Gaussian statistics'
         with
         P(q) = 2/{fv^2} * (v - (1-exp(-v)) + {f-1}/2 * (1-exp(-v))^2)
         where
         - v = u^2f/(3f-2)
         - u = <R_g^2>q^2, where <R_g^2> is the ensemble average radius of
-        giration squared of an arm
+        gyration squared of an arm
         - f is the number of arms on the star
         """
 category = "shape-independent"
 single = False
 # pylint: disable=bad-whitespace, line-too-long
 #             ["name", "units", default, [lower, upper], "type","description"],
-parameters = [["radius2", "Ang", 100.0, [0.0, inf], "", "Ensemble radius of gyration squared of an arm"],
+parameters = [["rg_squared", "Ang^2", 100.0, [0.0, inf], "", "Ensemble radius of gyration SQUARED of an arm"],
               ["arms",    "",      3,   [1.0, 6.0], "", "Number of arms in the model"],
              ]
 # pylint: enable=bad-whitespace, line-too-long
@@ -61,20 +65,21 @@ parameters = [["radius2", "Ang", 100.0, [0.0, inf], "", "Ensemble radius of gyra
 source = ["star_polymer.c"]
 
 demo = dict(scale=1, background=0,
-            radius2=100.0,
+            rg_squared=100.0,
             arms=3.0)
 
 oldname = 'StarPolymer'
 
-oldpars = dict(radius2='R2',
+oldpars = dict(rg_squared='R2',
                arms='arms')
 
-tests = [[{'radius2': 2.0,
+tests = [[{'rg_squared': 2.0,
            'arms':    3.3,
           }, 0.5, 0.851646091108],
 
-         [{'radius2':    1.0,
+         [{'rg_squared':    1.0,
            'arms':       2.0,
            'background': 1.8,
           }, 1.0, 2.53575888234],
         ]
+# 23Mar2016  RKH edited docs, would this better use rg not rg^2 ? Numerical noise at extremely small q.rg
