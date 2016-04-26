bfdfb23e61184d11705d084fd32d0b97cd87cfbe
Piotr Rozyczko <piotr.rozyczko@esss.se>
2016-04-26 12:10:01 +0200

    PK's code review for gen_so.py + local implementation of tgamma for compilers who don't support this method

--- gen_so.py
+++ gen_so.py
@@ -8,14 +8,11 @@ def main():
     from sasmodels import generate, core
 
     # Convert ../sasmodels/models/name.py to name
-    model_loc = os.path.join(sasmodels, "sasmodels", "models") 
-    for i in os.listdir(model_loc):
+    for model_name in core.list_models():
         # Choose only relevant python files
-        if i.endswith(".py") and not i.startswith("_"):
-            model_name = os.path.basename(i)[:-3]
-            model_info = core.load_model_info(model_name)
+            # model_info = core.load_model_info(model_name)
             # Run the conversion but don't delete the .so
-            model = core.build_model(model_info)
+            model = core.precompile_dll(model_name)
 
 if __name__ == "__main__":
     main()
