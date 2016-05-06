948b8c1c96e26f4920f18659f9e7a3970096c15b
Paul Kienzle <pkienzle@gmail.com>
2016-05-06 01:12:06 -0400

    simplify setup; read version from package init

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-import os
-from setuptools import setup,find_packages
+from distutils.core import setup
 
-if os.name == 'nt':
-    # Create the model .so's on windows
-    os.system("python gen_so.py")
-
-packages = find_packages(exclude=['contrib', 'docs', 'tests*'])
-package_data = {
-    'sasmodels.models': ['*.c','lib/*.c'],
-    'sasmodels': ['*.c'],
-    'models': ['*.so'],
-}
-required = []
+def find_version(package):
+    """Read package version string from __init__.py"""
+    import os
+    with open(os.path.join(package, '__init__.py')) as fid:
+        for line in fid.readlines():
+            if line.startswith('__version__'):
+                line = line[:line.find('#')]  # strip comment, if any
+                version = line.split('=', 2)[1].strip()
+                if version[0] != version[-1] or version[0] not in "\"'":
+                    break
+                return version[1:-1]
+    raise RuntimeError("Could not read version from %s/__init__.py"%package)
 
 setup(
-    name="sasmodels",
-    version = "1.0.0a",
-    description = "sasmodels package",
+    name='sasmodels',
+    version=find_version('sasmodels'),
+    description="sasmodels package",
     long_description=open('README.md').read(),
-    author = "SasView Collaboration",
-    author_email = "management@sasview.org",
-    url = "http://www.sasview.org",
-    keywords = "small-angle x-ray and neutron scattering analysis",
-    download_url = "https://github.com/SasView/sasmodels",
+    author="SasView Collaboration",
+    author_email="management@sasview.org",
+    url="http://www.sasview.org",
+    keywords="small-angle x-ray and neutron scattering analysis",
+    download_url="https://github.com/SasView/sasmodels",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -30,16 +30,25 @@ setup(
         'License :: Public Domain',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: C',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics',
     ],
-    packages=packages,
-    package_data=package_data,
-    install_requires = required,
+    packages=[
+        'sasmodels',
+        'sasmodels.models',
+        'sasmodels.custom'
+    ],
+    package_data={
+        'sasmodels.models': ['*.c', 'lib/*.c'],
+        'sasmodels': ['*.c', '*.cl', 'convert.json'],
+    },
+    install_requires = [
+    ],
     extras_require = {
         'OpenCL': ["pyopencl"],
         'Bumps': ["bumps"],
-        }
-
-    )
+        'TinyCC': ["tinycc"],
+    },
+)
