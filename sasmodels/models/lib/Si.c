a5b699717574561dff02253e45b39aa914df05cc
Paul Kienzle <pkienzle@nist.gov>
2016-10-14 18:07:28 -0400

    Si: Use Horner's method when computing the Sine integral

--- sasmodels/models/lib/Si.c
+++ sasmodels/models/lib/Si.c
@@ -2,24 +2,23 @@
 double Si(double x);
 double Si(double x)
 {
-    double out;
-
     if (x >= M_PI*6.2/4.0){
-        double out_sin = 0.0;
-        double out_cos = 0.0;
-        out = M_PI/2.0;
-
+        const double z = 1./(x*x);
         // Explicitly writing factorial values triples the speed of the calculation
-        out_cos = 1./x - 2./pow(x,3) + 24./pow(x,5) - 720./pow(x,7);
-        out_sin = 1./pow(x,2) - 6./pow(x,4) + 120./pow(x,6) - 5040./pow(x,8);
+        const double out_cos = (((-720.*z + 24.)*z - 2.)*z + 1.)/x;
+        const double out_sin = (((-5040.*z + 120.)*z - 6.)*z + 1)*z;
 
-        out -= cos(x) * out_cos;
-        out -= sin(x) * out_sin;
-        return out;
+        double cos_x, sin_x;
+        SINCOS(x, cos_x, sin_x);
+        return M_PI_2 - cos_x*out_cos - sin_x*out_sin;
+    } else {
+        const double z = x*x;
+        // Explicitly writing factorial values triples the speed of the calculation
+        return (((((-1./439084800.*z
+            + 1./3265920.)*z
+            - 1./35280.)*z
+            + 1./600.)*z
+            - 1./18.)*z
+            + 1.)*x;
     }
-
-    // Explicitly writing factorial values triples the speed of the calculation
-    out = x - pow(x, 3)/18. + pow(x,5)/600. - pow(x,7)/35280. + pow(x,9)/3265920. - pow(x,11)/439084800.;
-
-    return out;
 }
