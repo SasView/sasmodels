98ce141a6e091b02f8139ab3b235c16dafbce663
paul butler <butlerpd@udel.edu>
2016-11-27 16:25:24 -0500

    correct normalization problem in stacked_disk fixes #789. Also heavy cleanup and correction of equations to match code as well as standardization of the model documentation addressing #646.

--- sasmodels/models/stacked_disks.py
+++ sasmodels/models/stacked_disks.py
@@ -1,32 +1,29 @@
 r"""
-This model provides the form factor, $P(q)$, for stacked discs (tactoids)
-with a core/layer structure where the form factor is normalized by the volume
-of the cylinder. Assuming the next neighbor distance (d-spacing) in a stack
-of parallel discs obeys a Gaussian distribution, a structure factor $S(q)$
-proposed by Kratky and Porod in 1949 is used in this function.
-
-Note that the resolution smearing calculation uses 76 Gauss quadrature points
-to properly smear the model since the function is HIGHLY oscillatory,
-especially around the q-values that correspond to the repeat distance of
-the layers.
-
-The 2D scattering intensity is the same as 1D, regardless of the orientation
-of the q vector which is defined as
-
-.. math:: q = \sqrt{q_x^2 + q_y^2}
-
 Definition
 ----------
 
+This model provides the form factor, $P(q)$, for stacked discs (tactoids)
+with a core/layer structure which is constructed itself as $P(q) S(Q)$
+multiplying a $P(q)$ for individual core/layer disks by a structure factor
+$S(q)$ proposed by Kratky and Porod in 1949\ [#CIT1949]_ assuming the next
+neighbor distance (d-spacing) in the stack of parallel discs obeys a Gaussian
+distribution. As such the normalization of this "composite" form factor is
+relative to the individual disk volume, not the volume of the stack of disks.
+This model is appropriate for example for non non exfoliated clay particles such
+as Laponite.
+
 .. figure:: img/stacked_disks_geometry.png
+
+   Geometry of a single core/layer disk
 
 The scattered intensity $I(q)$ is calculated as
 
 .. math::
 
     I(q) = N\int_{0}^{\pi /2}\left[ \Delta \rho_t
-    \left( V_t f_t(q) - V_c f_c(q)\right) + \Delta \rho_c V_c f_c(q)
-    \right]^2 S(q)\sin{\alpha}\ d\alpha + \text{background}
+    \left( V_t f_t(q,\alpha) - V_c f_c(q,\alpha)\right) + \Delta
+    \rho_c V_c f_c(q,\alpha)\right]^2 S(q,\alpha)\sin{\alpha}\ d\alpha
+    + \text{background}
 
 where the contrast
 
@@ -34,79 +31,82 @@ where the contrast
 
     \Delta \rho_i = \rho_i - \rho_\text{solvent}
 
-and $N$ is the number of discs per unit volume,
-$\alpha$ is the angle between the axis of the disc and $q$,
-and $V_t$ and $V_c$ are the total volume and the core volume of
-a single disc, respectively.
+and $N$ is the number of individual (single) discs per unit volume, $\alpha$ is
+the angle between the axis of the disc and $q$, and $V_t$ and $V_c$ are the
+total volume and the core volume of a single disc, respectively, and
 
 .. math::
 
-    \left\langle f_{t}^2(q)\right\rangle_{\alpha} =
-    \int_{0}^{\pi/2}\left[
+    f_t(q,\alpha) =
     \left(\frac{\sin(q(d+h)\cos{\alpha})}{q(d+h)\cos{\alpha}}\right)
     \left(\frac{2J_1(qR\sin{\alpha})}{qR\sin{\alpha}} \right)
-    \right]^2 \sin{\alpha}\ d\alpha
 
-    \left\langle f_{c}^2(q)\right\rangle_{\alpha} =
-    \int_{0}^{\pi/2}\left[
+    f_c(q,\alpha) =
     \left(\frac{\sin(qh)\cos{\alpha})}{qh\cos{\alpha}}\right)
     \left(\frac{2J_1(qR\sin{\alpha})}{qR\sin{\alpha}}\right)
-    \right]^2 \sin{\alpha}\ d\alpha
 
 where $d$ = thickness of the layer (*thick_layer*),
 $2h$ = core thickness (*thick_core*), and $R$ = radius of the disc (*radius*).
 
 .. math::
 
-    S(q) = 1 + \frac{1}{2}\sum_{k=1}^n(n-k)\cos{(kDq\cos{\alpha})}
-    \exp\left[ -k(q\cos{\alpha})^2\sigma_d/2\right]
+    S(q,\alpha) = 1 + \frac{1}{2}\sum_{k=1}^n(n-k)\cos{(kDq\cos{\alpha})}
+    \exp\left[ -k(q)^2(D\cos{\alpha}~\sigma_d)^2/2\right]
 
 where $n$ is the total number of the disc stacked (*n_stacking*),
 $D = 2(d+h)$ is the next neighbor center-to-center distance (d-spacing),
 and $\sigma_d$ = the Gaussian standard deviation of the d-spacing (*sigma_d*).
+Note that $D\cos(\alpha)$ is the component of $D$ parallel to $q$ and the last
+term in the equation above is effectively a Debye-Waller factor term. 
 
 .. note::
-    Each assembly in the stack is layer/core/layer, so the spacing of the
+
+    1. Each assembly in the stack is layer/core/layer, so the spacing of the
     cores is core plus two layers. The 2nd virial coefficient of the cylinder
     is calculated based on the *radius* and *length*
     = *n_stacking* * (*thick_core* + 2 * *thick_layer*)
     values, and used as the effective radius for $S(Q)$ when $P(Q) * S(Q)$
     is applied.
 
+    2. the resolution smearing calculation uses 76 Gaussian quadrature points
+    to properly smear the model since the function is HIGHLY oscillatory,
+    especially around the q-values that correspond to the repeat distance of
+    the layers.
+
 To provide easy access to the orientation of the stacked disks, we define
 the axis of the cylinder using two angles $\theta$ and $\varphi$.
 
 .. figure:: img/cylinder_angle_definition.jpg
 
-    Examples of the angles against
-    the detector plane.
+    Examples of the angles against the detector plane.
 
 
-Our model uses the form factor calculations implemented in a c-library provided
-by the NIST Center for Neutron Research (Kline, 2006)
+Our model is derived from the form factor calculations implemented in a
+c-library provided by the NIST Center for Neutron Research
+(Kline, 2006)\ [#CIT_Kline]_
 
 References
 ----------
 
-A Guinier and G Fournet, *Small-Angle Scattering of X-Rays*,
-John Wiley and Sons, New York, 1955
+.. [#CIT1949] O Kratky and G Porod, *J. Colloid Science*, 4, (1949) 35
+.. [#CIT_Kline] S R Kline, *J Appl. Cryst.*, 39 (2006) 895
+.. [#] J S Higgins and H C Benoit, *Polymers and Neutron Scattering*,
+   Clarendon, Oxford, 1994
+.. [#] A Guinier and G Fournet, *Small-Angle Scattering of X-Rays*,
+   John Wiley and Sons, New York, 1955
 
-O Kratky and G Porod, *J. Colloid Science*, 4, (1949) 35
+Authorship and Verification
+----------------------------
 
-J S Higgins and H C Benoit, *Polymers and Neutron Scattering*,
-Clarendon, Oxford, 1994
-
-**Author:** NIST IGOR/DANSE **on:** pre 2010
-
-**Last Modified by:** Piotr Rozyczko **on:** February 18, 2016
-
-**Last Reviewed by:** Richard Heenan **on:** March 22, 2016
+* **Author:** NIST IGOR/DANSE **Date:** pre 2010
+* **Last Modified by:** Paul Butler and Paul Kienzle **on:** November 26, 2016
+* **Last Reviewed by:** Paul Butler and Paul Kienzle **on:** November 26, 2016
 """
 
 from numpy import inf
 
 name = "stacked_disks"
-title = ""
+title = "Form factor for a stacked set of non exfoliated core/shell disks"
 description = """\
     One layer of disk consists of a core, a top layer, and a bottom layer.
     radius =  the radius of the disk
