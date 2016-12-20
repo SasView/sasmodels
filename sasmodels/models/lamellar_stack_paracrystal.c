a0168e84cf86a1efabf5abe8430ab9266ddf1879
Paul Kienzle <pkienzle@nist.gov>
2016-12-20 17:16:46 -0500

    code cleanup; require at least one layer

--- sasmodels/models/lamellar_stack_paracrystal.c
+++ sasmodels/models/lamellar_stack_paracrystal.c
@@ -1,42 +1,30 @@
 /*	Lamellar_ParaCrystal - Pedersen's model
 
 */
-double Iq(double qval,
-      double th,
-      double Nlayers, 
-	  double davg, 
-	  double pd,
-      double sld,
-      double solvent_sld);
-double paraCryst_sn(double ww, double qval, double davg, long Nlayers, double an);
-double paraCryst_an(double ww, double qval, double davg, long Nlayers);
+double paraCryst_sn(double ww, double qval, double davg, int Nlayers, double an);
+double paraCryst_an(double ww, double qval, double davg, int Nlayers);
 
-double Iq(double qval,
-      double th,
-      double Nlayers, 
-	  double davg, 
-	  double pd,
-      double sld,
-      double solvent_sld)
+static double
+Iq(double qval,
+   double th,
+   double fp_Nlayers,
+   double davg,
+   double pd,
+   double sld,
+   double solvent_sld)
 {
-    
-	double inten,contr,xn;
-	double xi,ww,Pbil,Znq,Snq,an;
-	long n1,n2;
-	
-	contr = sld - solvent_sld;
 	//get the fractional part of Nlayers, to determine the "mixing" of N's
+	int n1 = (int)(fp_Nlayers);		//truncate towards zero
+	int n2 = n1 + 1;
+	const double xn = (double)n2 - fp_Nlayers;	//fractional contribution of n1
 	
-	n1 = (long)trunc(Nlayers);		//rounds towards zero
-	n2 = n1 + 1;
-	xn = (double)n2 - Nlayers;			//fractional contribution of n1
-	
-	ww = exp(-qval*qval*pd*pd*davg*davg/2.0);
+	const double ww = exp(-0.5*square(qval*pd*davg));
 
 	//calculate the n1 contribution
+	double Znq,Snq,an;
 	an = paraCryst_an(ww,qval,davg,n1);
 	Snq = paraCryst_sn(ww,qval,davg,n1,an);
-	
+
 	Znq = xn*Snq;
 	
 	//calculate the n2 contribution
@@ -51,29 +39,28 @@ double Iq(double qval,
 	//the limit when Nlayers approaches infinity
 //	Zq = (1-ww^2)/(1+ww^2-2*ww*cos(qval*davg))
 	
-	xi = th/2.0;		//use 1/2 the bilayer thickness
-	Pbil = (sin(qval*xi)/(qval*xi))*(sin(qval*xi)/(qval*xi));
+	const double xi = th/2.0;		//use 1/2 the bilayer thickness
+	const double Pbil = square(sinc(qval*xi));
 	
-	inten = 2.0*M_PI*contr*contr*Pbil*Znq/(qval*qval);
-	inten *= 1.0e-04;
+	const double contr = sld - solvent_sld;
+	const double inten = 2.0*M_PI*contr*contr*Pbil*Znq/(qval*qval);
 //printf("q=%.7e wwm1=%g ww=%.5e an=% 12.5e Snq=% 12.5e Znq=% 12.5e Pbil=% 12.5e\n",qval,wwm1,ww,an,Snq,Znq,Pbil);
-	return(inten);
+	return 1.0e-4*inten;
 }
 
 // functions for the lamellar paracrystal model
 double
-paraCryst_sn(double ww, double qval, double davg, long Nlayers, double an) {
+paraCryst_sn(double ww, double qval, double davg, int Nlayers, double an) {
 	
 	double Snq;
 
 	Snq = an/( (double)Nlayers*square(1.0+ww*ww-2.0*ww*cos(qval*davg)) );
 	
-	return(Snq);
+	return Snq;
 }
 
 double
-paraCryst_an(double ww, double qval, double davg, long Nlayers) {
-	
+paraCryst_an(double ww, double qval, double davg, int Nlayers) {
 	double an;
 	
 	an = 4.0*ww*ww - 2.0*(ww*ww*ww+ww)*cos(qval*davg);
@@ -81,6 +68,6 @@ paraCryst_an(double ww, double qval, double davg, long Nlayers) {
 	an += 2.0*pow(ww,(Nlayers+3))*cos((double)(Nlayers-1)*qval*davg);
 	an += 2.0*pow(ww,(Nlayers+1))*cos((double)(Nlayers+1)*qval*davg);
 	
-	return(an);
+	return an;
 }
 
