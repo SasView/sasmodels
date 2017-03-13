4f9e288196c233a7668a135cc94bb86a11da7958
paul butler <butlerpd@udel.edu>
2017-03-12 22:39:37 -0400

    update RPA documentation

--- sasmodels/models/rpa.py
+++ sasmodels/models/rpa.py
@@ -29,6 +29,17 @@ Case 9: A-B-C-D tetra-block copolymer
 .. note::
     These case numbers are different from those in the NIST SANS package!
 
+The models are based on the papers by Akcasu et al. [#Akcasu]_ and by
+Hammouda [#Hammouda]_ assuming the polymer follows Gaussian statistics such
+that $R_g^2 = n b^2/6$ where $b$ is the statistical segment length and $n$ is
+the number of statistical segment lengths. A nice tutorial on how these are
+constructed and implemented can be found in chapters 28 and 39 of Boualem
+Hammouda's 'SANS Toolbox'[#toolbox]_.
+
+In brief the macroscopic cross sections are derived from the general forms
+for homopolymer scattering and the multiblock cross-terms while the inter
+polymer cross terms are described in the usual way by the $\chi$ parameter.
+
 USAGE NOTES:
 
 * Only one case can be used at any one time.
@@ -38,17 +49,32 @@ USAGE NOTES:
   are calculated with respect to component D).** So the scattering contrast
   for a C/D blend = [SLD(component C) - SLD(component D)]\ :sup:`2`.
 * Depending on which case is being used, the number of fitting parameters can 
-  vary.  Note that in general the degrees of polymerization, the volume
-  fractions, the molar volumes, and the neutron scattering lengths for each
-  component are obtained from other methods and held fixed while the segment
-  lengths (b\ :sub:`a`, b\ :sub:`b`, etc) and $\chi$ parameters (K\ :sub:`ab`,
-  K\ :sub:`ac`, etc). The *scale* parameter should be held equal to unity.
+  vary.
+
+  .. Note::
+    * In general the degrees of polymerization, the volume
+      fractions, the molar volumes, and the neutron scattering lengths for each
+      component are obtained from other methods and held fixed while The *scale*
+      parameter should be held equal to unity.
+    * The variables are normally the segment lengths (b\ :sub:`a`, b\ :sub:`b`,
+      etc) and $\chi$ parameters (K\ :sub:`ab`, K\ :sub:`ac`, etc).
 
 
 References
 ----------
 
-.. [#] A Z Akcasu, R Klein and B Hammouda, *Macromolecules*, 26 (1993) 4136
+.. [#Akcasu] A Z Akcasu, R Klein and B Hammouda, *Macromolecules*, 26 (1993)
+   4136.
+.. [#Hammouda] B. Hammouda, *Advances in Polymer Science* 106 (1993) 87.
+.. [#toolbox] https://www.ncnr.nist.gov/staff/hammouda/the_sans_toolbox.pdf
+
+Authorship and Verification
+----------------------------
+
+* **Author:** Boualem Hammouda - NIST IGOR/DANSE **Date:** pre 2010
+* **Converted to sasmodels by:** Paul Kienzle **Date:** July 18, 2016
+* **Last Modified by:** Paul Butler **Date:** March 12, 2017
+* **Last Reviewed by:** Paul Butler **Date:** March 12, 2017
 """
 
 from numpy import inf
