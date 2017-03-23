f4f85b3a43d7e158d487593ba7eee6f3d8e98fc8
Paul Kienzle <pkienzle@nist.gov>
2017-03-22 21:31:01 -0400

    core shell bicelle elliptical: adapt to new orientation parameters; adjust effective shell radius in 2D model so it matches core shell bicelle when axis ratio is 1

--- sasmodels/models/core_shell_bicelle_elliptical.c
+++ sasmodels/models/core_shell_bicelle_elliptical.c
@@ -31,17 +31,16 @@ double form_volume(double radius, double x_core, double thick_rim, double thick_
     return M_PI*(radius+thick_rim)*(radius*x_core+thick_rim)*(length+2.0*thick_face);
 }
 
-double 
-                Iq(double qq,
-                   double rad,
-                   double x_core,
-                   double radthick,
-                   double facthick,
-                   double length,
-                   double rhoc,
-                   double rhoh,
-                   double rhor,
-                   double rhosolv)
+double Iq(double q,
+          double rad,
+          double x_core,
+          double radthick,
+          double facthick,
+          double length,
+          double rhoc,
+          double rhoh,
+          double rhor,
+          double rhosolv)
 {
     double si1,si2,be1,be2;
      // core_shell_bicelle_elliptical, RKH Dec 2016, based on elliptical_cylinder and core_shell_bicelle
@@ -73,8 +72,8 @@ double
         const double cos_alpha = ( Gauss76Z[i] + 1.0 )/2.0;
         const double sin_alpha = sqrt(1.0 - cos_alpha*cos_alpha);
         double inner_sum=0;
-        double sinarg1 = qq*halfheight*cos_alpha;
-        double sinarg2 = qq*(halfheight+facthick)*cos_alpha;
+        double sinarg1 = q*halfheight*cos_alpha;
+        double sinarg2 = q*(halfheight+facthick)*cos_alpha;
         si1 = sas_sinx_x(sinarg1);
         si2 = sas_sinx_x(sinarg2);
         for(int j=0;j<76;j++) {
@@ -82,8 +81,8 @@ double
             //const double beta = ( Gauss76Z[j]*(vbj-vaj) + vaj + vbj )/2.0;
             const double beta = ( Gauss76Z[j] +1.0)*M_PI_2;
             const double rr = sqrt(rA - rB*cos(beta));
-            double besarg1 = qq*rr*sin_alpha;
-            double besarg2 = qq*(rr+radthick)*sin_alpha;
+            double besarg1 = q*rr*sin_alpha;
+            double besarg2 = q*(rr+radthick)*sin_alpha;
             be1 = sas_2J1x_x(besarg1);
             be2 = sas_2J1x_x(besarg2);
             inner_sum += Gauss76Wt[j] *square(dr1*si1*be1 +
@@ -113,8 +112,8 @@ Iqxy(double qx, double qy,
           double psi)
 {
        // THIS NEEDS TESTING
-    double qq, cos_val, cos_mu, cos_nu;
-    ORIENT_ASYMMETRIC(qx, qy, theta, phi, psi, qq, cos_val, cos_mu, cos_nu);
+    double q, xhat, yhat, zhat;
+    ORIENT_ASYMMETRIC(qx, qy, theta, phi, psi, q, xhat, yhat, zhat);
     const double dr1 = rhoc-rhoh;
     const double dr2 = rhor-rhosolv;
     const double dr3 = rhoh-rhor;
@@ -124,14 +123,18 @@ Iqxy(double qx, double qy,
     const double vol2 = M_PI*(rad+radthick)*(radius_major+radthick)*2.0*(halfheight+facthick);
     const double vol3 = M_PI*rad*radius_major*2.0*(halfheight+facthick);
 
-    // Compute:  r = sqrt((radius_major*cos_nu)^2 + (radius_minor*cos_mu)^2)
+    // Compute:  r = sqrt((radius_major*zhat)^2 + (radius_minor*yhat)^2)
     // Given:    radius_major = r_ratio * radius_minor  
     // ASSUME the sin_alpha is included in the separate integration over orientation of rod angle
-    const double r = rad*sqrt(square(x_core*cos_nu) + cos_mu*cos_mu);
-    const double be1 = sas_2J1x_x( qq*r );
-    const double be2 = sas_2J1x_x( qq*(r + radthick ) );
-    const double si1 = sas_sinx_x( qq*halfheight*cos_val );
-    const double si2 = sas_sinx_x( qq*(halfheight + facthick)*cos_val );
+    const double rad_minor = rad;
+    const double rad_major = rad*x_core;
+    const double r_hat = sqrt(square(rad_major*xhat) + square(rad_minor*yhat));
+    const double rshell_hat = sqrt(square((rad_major+radthick)*xhat)
+                                   + square((rad_minor+radthick)*yhat));
+    const double be1 = sas_2J1x_x( q*r_hat );
+    const double be2 = sas_2J1x_x( q*rshell_hat );
+    const double si1 = sas_sinx_x( q*halfheight*zhat );
+    const double si2 = sas_sinx_x( q*(halfheight + facthick)*zhat );
     const double Aq = square( vol1*dr1*si1*be1 + vol2*dr2*si2*be2 +  vol3*dr3*si2*be1);
     //const double vol = form_volume(radius_minor, r_ratio, length);
     return 1.0e-4 * Aq;
