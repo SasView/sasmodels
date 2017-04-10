7c1cce33768834960ded2d38104d915782069119
Paul Kienzle <pkienzle@nist.gov>
2017-04-10 09:39:56 -0400

    oriented sesans: split transform into oriented and unoriented versions

--- sasmodels/sesans.py
+++ sasmodels/sesans.py
@@ -19,7 +19,7 @@ class SesansTransform(object):
     """
     Spin-Echo SANS transform calculator.  Similar to a resolution function,
     the SesansTransform object takes I(q) for the set of *q_calc* values and
-    produces a transformed dataset
+    produces a transformed dataset.
 
     *SElength* (A) is the set of spin-echo lengths in the measured data.
 
@@ -44,25 +44,14 @@ class SesansTransform(object):
         # type: (np.ndarray, float, float) -> None
         #import logging; logging.info("creating SESANS transform")
         self.q = z
-        # isoriented flag determines whether data is from an oriented sample or not, should be a selection variable upon entering SESANS data.
-        if self.isoriented==False:
-            self._set_hankel(SElength, zaccept, Rmax)
-        if self.isoriented==True:
-            self._set_cosmat(SElength, zaccept, Rmax)
+        # isoriented flag determines whether data is from an oriented sample or
+        # not, should be a selection variable upon entering SESANS data.
+        self._set_hankel(SElength, zaccept, Rmax)
 
     def apply(self, Iq):
-        try:
-            G0 = np.dot(self._H0, Iq)
-            G = np.dot(self._H.T, Iq)
-            P = G - G0
-        except:
-            try:
-                dq = self.q_calc[0]
-                G0 = sum(np.dot(self._cos0, Iq) * dq)
-                G = sum(np.dot(self._cosmat.T, Iq) * dq)
-                P = G - G0
-            except:
-                raise ValueError('Sesanstransform.apply cannot generate either a Hankel transform or a cosine transform of you SESANS data')
+        G0 = np.dot(self._H0, Iq)
+        G = np.dot(self._H.T, Iq)
+        P = G - G0
         return P
 
     def _set_hankel(self, SElength, zaccept, Rmax):
@@ -85,6 +74,48 @@ class SesansTransform(object):
         self.q_calc = q
         self._H, self._H0 = H, H0
 
+class OrientedSesansTransform(object):
+    """
+    Oriented Spin-Echo SANS transform calculator.  Similar to a resolution
+    function, the OrientedSesansTransform object takes I(q) for the set
+    of *q_calc* values and produces a transformed dataset.
+
+    *SElength* (A) is the set of spin-echo lengths in the measured data.
+
+    *zaccept* (1/A) is the maximum acceptance of scattering vector in the spin
+    echo encoding dimension (for ToF: Q of min(R) and max(lam)).
+
+    *Rmax* (A) is the maximum size sensitivity; larger radius requires more
+    computation time.
+    """
+    #: SElength from the data in the original data units; not used by transform
+    #: but the GUI uses it, so make sure that it is present.
+    q = None  # type: np.ndarray
+
+    #: q values to calculate when computing transform
+    q_calc = None  # type: np.ndarray
+
+    # transform arrays
+    _cosmat = None  # type: np.ndarray
+    _cos0 = None # type: np.ndarray
+    _Iq_shape = None # type: Tuple[int, int]
+
+    def __init__(self, z, SElength, zaccept, Rmax):
+        # type: (np.ndarray, float, float) -> None
+        #import logging; logging.info("creating SESANS transform")
+        self.q = z
+        # isoriented flag determines whether data is from an oriented sample or
+        # not, should be a selection variable upon entering SESANS data.
+        self._set_cosmat(SElength, zaccept, Rmax)
+
+    def apply(self, Iq):
+        dq = self.q_calc[0][0]
+        Iq = np.reshape(Iq, self._Iq_shape)
+        G0 = self._cos0 * np.sum(Iq) * dq
+        G = np.sum(np.dot(Iq, self._cosmat.T), axis=1) * dq
+        P = G - G0
+        return P
+
     def _set_cosmat(self, SElength, zaccept, Rmax):
         # type: (np.ndarray, float, float) -> None
         # Force float32 arrays, otherwise run into memory problems on some machines
@@ -98,10 +129,9 @@ class SesansTransform(object):
         dq = q_min
 
         cos0 = np.float32(dq / (2 * pi))
+        cosmat = np.float32(dq / (2 * pi)) * np.cos(q[:, None] * SE[None, :])
 
-        repq = np.tile(q, (SElength.size, 1)).T
-        repSE = np.tile(SElength, (q.size, 1))
-        cosmat = np.float32(dq / (2 * pi)) * np.cos(repSE * repq)
-
-        self.q_calc = q
+        qx, qy = np.meshgrid(q, q)
+        self._Iq_shape = qx.shape
+        self.q_calc = qx.flatten(), qy.flatten()
         self._cosmat, self._cos0 = cosmat, cos0
