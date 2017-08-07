9826f822b572af636df38b920713fdbcccedc0ff
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2017-08-07 12:26:04 +0100

    Fix import on Python 3

--- sasmodels/model_test.py
+++ sasmodels/model_test.py
@@ -46,7 +46,11 @@ from __future__ import print_function
 
 import sys
 import unittest
-from StringIO import StringIO
+
+try:
+    from StringIO import StringIO
+except ImportError: # StringIO.StringIO renamed to io.StringIO in Python 3
+    from io import StringIO
 
 import numpy as np  # type: ignore
 
