3f9db6ed1e057839dbf05e4bed09ac980021463c
Omer Eisenberg <omereis@yahoo.com>
2017-12-08 11:06:05 -0500

    supporting all sorts of special power: +/- 1/3, 1/2, 2, 3

--- sasmodels/py2c.py
+++ sasmodels/py2c.py
@@ -112,6 +112,13 @@ def to_source(node, func_name):
 #    return ''.join(generator.result)
     return ''.join(generator.c_proc)
 
+def isevaluable(s):
+    try:
+        eval(s)
+        return True
+    except:
+        return False
+    
 class SourceGenerator(NodeVisitor):
     """This visitor is able to transform a well formed syntax tree into python
     sourcecode.  For more details have a look at the docstring of the
@@ -777,33 +784,48 @@ class SourceGenerator(NodeVisitor):
             self.visit(value)
         self.write_python('}')
 
+    def get_special_power (self, string):
+        function_name = ''
+        is_negative_exp = False
+        if (isevaluable(str(self.current_statement))):
+            exponent = eval(string)
+            is_negative_exp = exponent < 0
+            abs_exponent = abs(exponent)
+            if (abs_exponent == 2):
+                function_name = "square"
+            elif (abs_exponent == 3):
+                function_name = "cube"
+            elif (abs_exponent == 0.5):
+                function_name = "sqrt"
+            elif (abs_exponent == 1.0/3.0):
+                function_name = "cbrt"
+        if (function_name == ''):
+            function_name = "pow"
+        return function_name, is_negative_exp
+
     def translate_power (self, node):
 # get exponent by visiting the right hand argument.
         function_name = "pow"
         temp_statement = self.current_statement
+# 'visit' functions write the results to the 'current_statement' class memnber
+# Here, a temporary variable, 'temp_statement', is used, that enables the
+# use of the 'visit' function
         self.current_statement = ''
         self.visit(node.right)
         exponent = self.current_statement.replace(' ','')
-        exponent = exponent.replace('(','')
-        exponent = exponent.replace(')','')
+        function_name, is_negative_exp = self.get_special_power (self.current_statement)
         self.current_statement = temp_statement
-# is the right hand argument, the exponent, a number?
-        if (hasattr(node.right, 'n')): # power of constand
-            exponent = node.right.n
-            if (exponent == 2):
-                function_name = "square"
-            elif (exponent == 3):
-                function_name = "cube"
-            elif (exponent == 0.5):
-                function_name = "sqrt"
-        elif (exponent == "1/2"):
-                function_name = "sqrt"
+        if (is_negative_exp):
+            self.write_c ("1.0 / (")
         self.write_c (function_name + " (")
         self.visit(node.left)
         if (function_name == "pow"):
             self.write_c(", ")
             self.visit(node.right)
         self.write_c(")")
+        if (is_negative_exp):
+            self.write_c(")")
+        self.write_c(" ")
 
     def translate_integer_divide (self, node):
         self.write_c ("(int) (")
