88096f552748890cc8b1ab0836eb234c560caf06
Paul Kienzle <pkienzle@nist.gov>
2018-01-16 12:14:15 -0500

    back out attempted marketplace update fix

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,5 @@ then
     chmod 600 .travis/travis_rsa
     ssh-add .travis/travis_rsa
     git remote add deploy git@danse.chem.utk.edu:/home/git/sasmodels
-    # Travis is reporting: [remote rejected] master -> master (shallow update not allowed)
-    # Maybe the following will fix it?
-    # https://stackoverflow.com/questions/28983842/remote-rejected-shallow-update-not-allowed-after-changing-git-remote-url#28985327
-    git fetch --unshallow deploy
     git push deploy master
 fi
