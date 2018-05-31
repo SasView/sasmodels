7994359d6c078cf35c29184c8eb57657f44ef374
Paul Butler <butlerpd@udel.edu>
2018-05-31 01:00:58 -0400

    Update ref and author to standard format

--- sasmodels/models/mass_surface_fractal.py
+++ sasmodels/models/mass_surface_fractal.py
@@ -50,13 +50,15 @@ and $\rho_{particle}$ is the scattering length density of particles.
 References
 ----------
 
-P Schmidt, *J Appl. Cryst.*, 24 (1991) 414-435 Equation(19)
+.. [#] P Schmidt, *J Appl. Cryst.*, 24 (1991) 414-435 Equation(19)
+.. [#] A J Hurd, D W Schaefer, J E Martin, *Phys. Rev. A*,
+   35 (1987) 2361-2364 Equation(2)
 
-A J Hurd, D W Schaefer, J E Martin, *Phys. Rev. A*,
-35 (1987) 2361-2364 Equation(2)
+Authorship and Verification
+----------------------------
 
+* **Converted to sasmodels by:** Piotr Rozyczko **Date:** Jan 20, 2016
 * **Last Reviewed by:** Richard Heenan **Date:** May 30, 2018
-
 """
 
 import numpy as np
