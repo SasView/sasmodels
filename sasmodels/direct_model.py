b6d1d529a295bd78ca9ec046f8d9ceb4b6741f05
Paul Kienzle <pkienzle@gmail.com>
2018-09-27 22:47:35 -0400

    use default background from kernel for direct model and bumps model

--- sasmodels/direct_model.py
+++ sasmodels/direct_model.py
@@ -30,7 +30,6 @@ from . import weights
 from . import resolution
 from . import resolution2d
 from .details import make_kernel_args, dispersion_mesh
-from .modelinfo import DEFAULT_BACKGROUND
 
 # pylint: disable=unused-import
 try:
@@ -349,7 +348,8 @@ class DataMixin(object):
             self._kernel = self._model.make_kernel(self._kernel_inputs)
 
         # Need to pull background out of resolution for multiple scattering
-        background = pars.get('background', DEFAULT_BACKGROUND)
+        default_background = self._model.info.parameters.common_parameters[1].default
+        background = pars.get('background', default_background)
         pars = pars.copy()
         pars['background'] = 0.
 
