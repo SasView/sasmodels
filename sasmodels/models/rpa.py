055ec4fc3a36fe5d3ff655931eb83565f03cf269
Steve King <stephen.king@stfc.ac.uk>
2019-03-27 10:56:00 +0000

    Tweaks to docs

--- sasmodels/models/rpa.py
+++ sasmodels/models/rpa.py
@@ -29,15 +29,15 @@ Case 9: A-B-C-D tetra-block copolymer
 .. note::
     These case numbers are different from those in the NIST SANS package!
 
-The models are based on the papers by Akcasu *et al.* and by
-Hammouda assuming the polymer follows Gaussian statistics such
+The models are based on the papers by Akcasu *et al.* [1] and by
+Hammouda [2] assuming the polymer follows Gaussian statistics such
 that $R_g^2 = n b^2/6$ where $b$ is the statistical segment length and $n$ is
 the number of statistical segment lengths. A nice tutorial on how these are
-constructed and implemented can be found in chapters 28 and 39 of Boualem
-Hammouda's 'SANS Toolbox'.
+constructed and implemented can be found in chapters 28, 31 and 34, and Part H, 
+of Hammouda's 'SANS Toolbox' [3].
 
-In brief the macroscopic cross sections are derived from the general forms
-for homopolymer scattering and the multiblock cross-terms while the inter
+In brief, the macroscopic cross sections are derived from the general forms
+for homopolymer scattering and the multiblock cross-terms while the inter,
 polymer cross terms are described in the usual way by the $\chi$ parameter.
 
 USAGE NOTES:
@@ -47,7 +47,7 @@ USAGE NOTES:
   polymer mixture is in the homogeneous mixed-phase region.
 * **Component D is assumed to be the "background" component (ie, all contrasts
   are calculated with respect to component D).** So the scattering contrast
-  for a C/D blend = [SLD(component C) - SLD(component D)]\ :sup:`2`.
+  for a C/D blend $\rho_{C/D} = [\rho_C - \rho_D]$\ :sup:`2`.
 * Depending on which case is being used, the number of fitting parameters can
   vary.
 
@@ -79,7 +79,7 @@ Authorship and Verification
 * **Author:** Boualem Hammouda - NIST IGOR/DANSE **Date:** pre 2010
 * **Converted to sasmodels by:** Paul Kienzle **Date:** July 18, 2016
 * **Last Modified by:** Paul Butler **Date:** March 12, 2017
-* **Last Reviewed by:** Paul Butler **Date:** March 12, 2017
+* **Last Reviewed by:** Steve King **Date:** March 27, 2019
 * **Source added by :** Steve King **Date:** March 25, 2019
 """
 
