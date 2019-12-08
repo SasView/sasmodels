d7f3575c208f01231de1541062f3c0e1060e359d
Stuart Prescott <stuart@debian.org>
2019-12-08 12:04:26 +1100

    Fix URL to cited thesis

--- sasmodels/models/core_shell_parallelepiped.py
+++ sasmodels/models/core_shell_parallelepiped.py
@@ -170,8 +170,7 @@ References
    Equations (1), (13-14). (in German)
 #. D Singh (2009). *Small angle scattering studies of self assembly in
    lipid mixtures*, Johns Hopkins University Thesis (2009) 223-225. `Available
-   from Proquest <http://search.proquest.com/docview/304915826?accountid
-   =26379>`_
+   from Proquest <http://search.proquest.com/docview/304915826>`_
 #.  L. Onsager, *Ann. New York Acad. Sci.*, 51 (1949) 627-659
 
 Authorship and Verification
