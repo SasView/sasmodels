9da4db3918752383dba765d804c92daa27f53ecc
Paul Butler <butlerpd@udel.edu>
2020-03-25 21:05:37 -0400

    First Pass at updating Porod docs (again)
    Now that we understand much better the interplay between this model and
    the invariant panel (though not clear why we are doing things this way
    now). Still need to edit the second NOTE with the caveats of using the
    function.

--- sasmodels/models/porod.py
+++ sasmodels/models/porod.py
@@ -1,13 +1,46 @@
 r"""
-This model fits the Porod function
+This model is a special case of the power law model, deriving its special name
+from the Porod Law which it models, where the power law exponent is fixed to -4.
 
-.. math:: I(q) = C/q^4
+.. math:: I(q) = C/q^4 + Background
 
-to the data directly without any need for linearisation (cf. Log I(q) vs Log q).
+Where the power law constant $C$ in this case is just the scale factor. 
 
-In the limit as q $\to \infty$ the scale factor $C = 2\pi (\Delta\rho)^2 S_v$
-where $S_v$ is the specific surface area (ie, surface area / volume) of the sample,
-and $\Delta\rho$ is the contrast factor.
+The Porod law, similar to the Guinier model, is a broadly applicable model to a
+very restricted portion of the data. While the Guinier model applies to any
+dilute particulate system regardless of shape or size but only to the very low
+$q$ (being a taylor expansion around $q$ = 0), the Porod Law applies to any
+scattering system with sharp scattering length density interfaces between the
+phases but only at very high $q$ (technically in the limit as $q \to \infty$ ..
+except of course that, practically, the continum approach breaks down there).
+It is based on the idea that at sufficiently high $q$ there is no shape
+information left and all scattering is just reflections off the sharp
+interfaces.
+
+In the special case of a two phase system, the power law constant $C$ derived
+from the appropriate $Q$ limit portion of the data is known as the Porod
+Constant and can be written as:
+
+.. math:: C = 2\pi (\Delta\rho)^2 S_v
+
+where $S_v$ is the specific surface area (ie, surface area / volume) of the
+material under study, and $\Delta\rho$ is the contrast factor between the two
+phases.
+
+Thus, by extracting the Porod constant from experimental data, and
+knowing the contrast factor between the two phases, one can obtain the specific
+surface area for the material. This can be very useful for example in
+understanding porosoity in materials, particularly when used in conjunction
+with complementary techniques such as BET.
+
+.. Note:: The Invariant analysis panel will compute the Sv if the value of
+    the Porod constant (obtained for example from this fit) is entered into the
+    appropriate place.
+
+
+.. Note:: There are several caveats regarding obtaining a good experimental
+    value of the Porod constant. The first is that you must have sufficiently
+    large $q$ that you are in the Porod region.
 
 For 2D data: The 2D scattering intensity is calculated in the same way as 1D,
 where the q vector is defined as
