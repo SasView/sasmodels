d1574e97c4a085b437b11f318f64833a120352d2
dehoni <dirk.honecker@stfc.ac.uk>
2022-06-21 16:55:29 +0100

    Revert "c-fy broad_peak"
    This reverts commit c6a92619a20b0e7f49666e1dc7338567d2508fd3.

--- sasmodels/models/broad_peak.py
+++ sasmodels/models/broad_peak.py
@@ -67,14 +67,38 @@ category = "shape-independent"
 parameters = [["porod_scale",    "",  1.0e-05, [-inf, inf], "", "Power law scale factor"],
               ["porod_exp",      "",      3.0, [-inf, inf], "", "Exponent of power law"],
               ["peak_scale",  "",     10.0, [-inf, inf], "", "Scale factor for broad peak"],
-              ["correlation_length", "Ang",  50.0, [0, inf], "", "screening length"],
+              ["correlation_length", "Ang",  50.0, [-inf, inf], "", "screening length"],
               ["peak_pos",       "1/Ang", 0.1, [-inf, inf], "", "Peak position in q"],
               ["width_exp",    "",      2.0, [-inf, inf], "", "Exponent of peak width"],
               ["shape_exp",    "",      1.0, [-inf, inf], "", "Exponent of peak shape"],              
              ]
 # pylint: enable=bad-whitespace, line-too-long
 
-source = ["broad_peak.c"]
+def Iq(q,
+       porod_scale=1.0e-5,
+       porod_exp=3.0,
+       peak_scale=10.0,
+       correlation_length=50.0,
+       peak_pos=0.1,
+       width_exp=2.0,
+       shape_exp=1.0):
+    """
+    :param q:                    Input q-value
+    :param porod_scale:          Power law scale factor
+    :param porod_exp:            Exponent of power law
+    :param peak_scale:           Scale factor for broad peak
+    :param correlation_length:   Correlation length
+    :param peak_pos:             Peak position in q
+    :param width_exp:            Exponent of peak width
+    :param shape_exp:            Exponent of peak shape    
+    :return:                     Calculated intensity
+    """
+    z = abs(q - peak_pos) * correlation_length
+    with errstate(divide='ignore'):
+        inten = (porod_scale / q ** porod_exp
+                 + peak_scale / (1 + z ** width_exp)) ** shape_exp
+    return inten
+Iq.vectorized = True  # Iq accepts an array of q values
 
 def random():
     """Return a random parameter set for the model."""
