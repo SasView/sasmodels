1d98b952b6017d6cc47932b24ffbcdcf208365ca
Liu <yunliu@nist.gov>
2024-01-21 14:28:59 -0500

    Update the python code of the two-Yukawa code. (This verserion still uses the c-code to calcualte the structure factor.)

--- sasmodels/models/TY_YukawaSq.py
+++ sasmodels/models/TY_YukawaSq.py
@@ -21,13 +21,18 @@ from sasmodels.special import *
 from numpy import inf
 
 name = "TY_YukawaSq"
-title = "User model for two Yukiawa structure factor (S(q))"
+title = "User model for two Yukawa structure factor (S(q))"
 description = """"""
+
+category = "structure-factor"
+structure_factor = True
+
+single = False
 
 parameters = [ 
 #   ["name", "units", default, [lower, upper], "type", "description"],
-    ['radius', '', 50.0, [-inf, inf], '', ''],
-    ['volumefraction', '', 0.1, [-inf, inf], '', ''],
+    ["radius_effective", "Ang", 50.0, [-inf, inf], '', ''],
+    ['volfraction', '', 0.1, [-inf, inf], '', ''],
     ['k1', '', 6, [-inf, inf], '', ''],
     ['k2', '', -2.0, [-inf, inf], '', ''],
     ['z1', '', 10.0, [-inf, inf], '', ''],
@@ -47,10 +52,8 @@ source = ["TY_PairCorrelation.c", "TY_cpoly.c","TY_TwoYukawa.c", "TY_utility.c",
 
 #source = ["test2yv2_inc.c"]
 
-
-structre_factor = True
 #haveFq = True   # for beta calculation
-single = False  # make sure that it has double digit precision
+#single = False  # make sure that it has double digit precision
 #opencl = False  # related with parallel computing
 
 #Iq.vectorized = True
