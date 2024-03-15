510234f75afc8bc2f0bf951bf357d14f28e6854c
Paul Kienzle <pkienzle@nist.gov>
2024-03-15 15:05:07 -0400

    need .h files from models directory for two yukawa

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         'sasmodels.custom'
     ],
     package_data={
-        'sasmodels.models': ['*.c', 'lib/*.c', 'lib/*.h'],
+        'sasmodels.models': ['*.c', '*.h', 'lib/*.c', 'lib/*.h'],
         'sasmodels': ['*.c', '*.cl'],
     },
     install_requires=install_requires,
