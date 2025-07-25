8353f56a716d87498ac1b242696de21e01cd09eb
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-07-25 16:12:27 +0100

    Removes pylab from "sesansdemo.py"

--- sesansdemo.py
+++ sesansdemo.py
@@ -5,12 +5,14 @@
 
 from __future__ import division
 
-from pylab import *
+#from pylab import *
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.special import jv as besselj
 
 # q-range parameters
 
-q = arange(0.0003, 1.0, 0.0003)    # [nm^-1] range wide enough for  Hankel transform
+q = np.arange(0.0003, 1.0, 0.0003)    # [nm^-1] range wide enough for  Hankel transform
 dq=(q[1]-q[0])*1e9   # [m^-1] step size in q, needed for integration
 nq=len(q)
 Lambda=2e-10   # [m] wavelength
@@ -18,7 +20,7 @@ Lambda=2e-10   # [m] wavelength
 phi=0.1   # volume fraction
 R=100       # [nm] radius particles
 DeltaRho=6e14  # [m^-2]
-V=4/3*pi*R**3 * 1e-27 # [m^3]
+V=4/3*np.pi*R**3 * 1e-27 # [m^3]
 th=0.002    # [m] thickness sample
 
 #2 PHASE SYSTEM
@@ -26,60 +28,60 @@ st= 1.5*Lambda**2*DeltaRho**2*th*phi*(1-phi)*R*1e-9  # scattering power in sesan
 
 # Form factor solid sphere
 qr=q*R
-P=(3.*(sin(qr)-qr*cos(qr)) / qr**3)**2
+P=(3.*(np.sin(qr)-qr*np.cos(qr)) / qr**3)**2
 # Structure factor dilute
 S=1.
 #2 PHASE SYSTEM
 # scattered intensity [m^-1] in absolute units according to SANS 
 I=phi*(1-phi)*V*(DeltaRho**2)*P*S
 
-clf()
-subplot(211)  # plot the SANS calculation
-plot(q,I,'k')
-loglog(q,I)
-xlim([0.01, 1])
-ylim([1, 1e9])
-xlabel(r'$Q [nm^{-1}]$')
-ylabel(r'$d\Sigma/d\Omega [m^{-1}]$')
+plt.clf()
+plt.subplot(211)  # plot the SANS calculation
+plt.plot(q,I,'k')
+plt.loglog(q,I)
+plt.xlim([0.01, 1])
+plt.ylim([1, 1e9])
+plt.xlabel(r'$Q [nm^{-1}]$')
+plt.ylabel(r'$d\Sigma/d\Omega [m^{-1}]$')
 
 
 # Hankel transform to nice range for plot
 nz=61
-zz=linspace(0,240,nz) # [nm], should be less than reciprocal from q
-G=zeros(nz)
+zz=np.linspace(0,240,nz) # [nm], should be less than reciprocal from q
+G=np.zeros(nz)
 for i in range(len(zz)):
     integr=besselj(0,q*zz[i])*I*q
-    G[i]=sum(integr)
-G=G*dq*1e9*2*pi # integr step, conver q into [m**-1] and 2 pi circle integr
+    G[i]=np.sum(integr)
+G=G*dq*1e9*2*np.pi # integr step, conver q into [m**-1] and 2 pi circle integr
 # plot(zz,G);
-stt= th*Lambda**2/4/pi/pi*G[0]  # scattering power according to SANS formalism
-PP=exp(th*Lambda**2/4/pi/pi*(G-G[0]))
+stt= th*Lambda**2/4/np.pi/np.pi*G[0]  # scattering power according to SANS formalism
+PP=np.exp(th*Lambda**2/4/np.pi/np.pi*(G-G[0]))
 
-subplot(212)
-plot(zz,PP,'k',label="Hankel transform") # Hankel transform 1D
-xlabel('spin-echo length [nm]')
-ylabel('polarisation normalised')
-hold(True)
+plt.subplot(212)
+plt.plot(zz,PP,'k',label="Hankel transform") # Hankel transform 1D
+plt.xlabel('spin-echo length [nm]')
+plt.ylabel('polarisation normalised')
+#plt.hold(True)
 
 # Cosine transformation of 2D scattering patern
 if False:
-    qy,qz = meshgrid(q,q)
-    qr=R*sqrt(qy**2 + qz**2) # reuse variable names Hankel transform, but now 2D
-    P=(3.*(sin(qr)-qr*cos(qr)) / qr**3)**2
+    qy,qz = np.meshgrid(q,q)
+    qr=R*np.sqrt(qy**2 + qz**2) # reuse variable names Hankel transform, but now 2D
+    P=(3.*(np.sin(qr)-qr*np.cos(qr)) / qr**3)**2
     # Structure factor dilute
     S=1.
     # scattered intensity [m^-1] in absolute units according to SANS
     I=phi*V*(DeltaRho**2)*P*S
-    GG=zeros(nz)
+    GG=np.zeros(nz)
     for i in range(len(zz)):
-        integr=cos(qz*zz[i])*I
+        integr=np.cos(qz*zz[i])*I
         GG[i]=sum(sum(integr))
     GG=4*GG* dq**2 # take integration step into account take 4 quadrants
     # plot(zz,GG);
-    sstt= th*Lambda**2/4/pi/pi*GG[0]  # scattering power according to SANS formalism
-    PPP=exp(th*Lambda**2/4/pi/pi*(GG-GG[0]))
+    sstt= th*Lambda**2/4/np.pi/np.pi*GG[0]  # scattering power according to SANS formalism
+    PPP=np.exp(th*Lambda**2/4/np.pi/np.pi*(GG-GG[0]))
 
-    plot(zz,PPP,label="cosine transform") # cosine transform 2D
+    plt.plot(zz,PPP,label="cosine transform") # cosine transform 2D
 
 # For comparison calculation in SESANS formalism, which overlaps perfectly
 def gsphere(z,r):
@@ -89,16 +91,16 @@ def gsphere(z,r):
     Wim Bouwman after formulae Timofei Kruglov J.Appl.Cryst. 2003 article
     """
     d = z/r
-    g = zeros_like(z)
+    g = np.zeros_like(z)
     g[d==0] = 1.
     low = ((d > 0) & (d < 2))
     dlow = d[low]
     dlow2 = dlow**2
     print(dlow.shape, dlow2.shape)
-    g[low] = sqrt(1-dlow2/4.)*(1+dlow2/8.) + dlow2/2.*(1-dlow2/16.)*log(dlow/(2.+sqrt(4.-dlow2)))
+    g[low] = np.sqrt(1-dlow2/4.)*(1+dlow2/8.) + dlow2/2.*(1-dlow2/16.)*np.log(dlow/(2.+np.sqrt(4.-dlow2)))
     return g
 
 if True:
-    plot(zz,exp(st*(gsphere(zz,R)-1)),'r', label="analytical")
-legend()
-show()
+    plt.plot(zz,np.exp(st*(gsphere(zz,R)-1)),'r', label="analytical")
+plt.legend()
+plt.show()
