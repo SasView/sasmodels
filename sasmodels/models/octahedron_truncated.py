f11bc682418c7d634f0f292dde0eebe52e5c06de
Wojciech Potrzebowski <wojtek.potrzebowski@scilifelab.se>
2025-11-15 09:49:06 +0100

    Update octahedron_truncated.py

--- sasmodels/models/octahedron_truncated.py
+++ sasmodels/models/octahedron_truncated.py
@@ -77,7 +77,9 @@ Amplitude of the form factor AP for the fixed orientation of the shape reads::
     CC = 1./(2*(qx*qx-qy*qy)*(qx*qx-qz*qz))*((qx-qz)*sin(qx*(1.-t)-qz*t)+(qx+qz)*sin(qx*(1.-t)+qz*t)) +
         1./(2*(qy*qy-qz*qz)*(qy*qy-qx*qx))*((qy-qz)*sin(qy*(1.-t)-qz*t)+(qy+qz)*sin(qy*(1.-t)+qz*t));
 
-    and
+where:
+
+.. code-block:: text
 
     Qx = q * sin_theta * cos_phi;
     Qy = q * sin_theta * sin_phi;
