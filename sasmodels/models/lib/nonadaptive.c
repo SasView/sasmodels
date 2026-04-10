dea77dd5bdc526718de85eabdc63e17af135d444
Paul Kienzle <pkienzle@nist.gov>
2026-04-09 21:12:03 -0400

    add missing lib/nonadaptive.c which is used for sasmodels.compare accuracy tests

---
+++ sasmodels/models/lib/nonadaptive.c
@@ -0,0 +1,11 @@
+// To force a fixed rather than adaptive integration scheme, replace [..., "lib/adaptive.c", ...]
+// with [..., "lib/gauss<n>.c", "lib/nonadaptive.c", ...] in your source lists.
+
+#define GAUSS_76 0.0 // Doesn't matter since qr is ignored
+#define GAUSS_500 0.0 // Doesn't matter since qr is ignored
+
+static int
+gauss_weights(double qr, constant double **w, constant double **z)
+{
+    *w = GAUSS_W; *z = GAUSS_Z; return GAUSS_N;
+}
