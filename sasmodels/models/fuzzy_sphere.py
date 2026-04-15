8bc1290b65a420803c388d2dfee58f4bf40611c5
Steve K <smk78@users.noreply.github.com>
2026-04-15 18:02:01 +0100

    Rewritten documentation

--- sasmodels/models/fuzzy_sphere.py
+++ sasmodels/models/fuzzy_sphere.py
@@ -4,43 +4,65 @@ the :ref:`magnetism` documentation.
 
 Definition
 ----------
+Scattering from spheres with a Gaussian interface.
 
 The scattering intensity $I(q)$ is calculated as:
 
 .. math::
 
-    I(q) = \frac{\text{scale}}{V}(\Delta \rho)^2 A^2(q) S(q)
+    I(q) = \frac{\text{scale}}{V}(\Delta \rho)^2 A(q)^2 S(q)
            + \text{background}
 
-
-where the amplitude $A(q)$ is given as the typical sphere scattering convoluted
-with a Gaussian to get a gradual drop-off in the scattering length density:
+where the amplitude $A(q)$ describes the radial scattering length density
+profile of a homogeneous sphere convoluted with a Gaussian in order to give
+a function with a gradual drop-off in scattering length density (SLD) towards
+the interface (i.e. a sphere with a diffuse, or "fuzzy", interface):
 
 .. math::
 
     A(q) = \frac{3\left[\sin(qR) - qR \cos(qR)\right]}{(qR)^3}
            \exp\left(\frac{-(\sigma_\text{fuzzy}q)^2}{2}\right)
 
-Here $A(q)^2$ is the form factor, $P(q)$. The scale is equivalent to the
-volume fraction of spheres, each of volume, $V$. Contrast $(\Delta \rho)$
-is the difference of scattering length densities of the sphere and the
-surrounding solvent.
+Here $A(q)^2$ is the form factor, $P(q)$. The $scale$ is equivalent to the
+volume fraction of spheres, each of volume, $V$. And the contrast $(\Delta \rho)$
+is the difference in SLD between a sphere and the surrounding medium.
 
-Poly-dispersion in radius and in fuzziness is provided for, though the
-fuzziness must be kept much smaller than the sphere radius for meaningful
-results.
+In this model, **$R$ represents the radius at which the SLD has decreased to
+half of its value at the core, not the overall radius of a sphere**. This is
+a frequent source of confusion when applying this model.
+  
+$\sigma_\text{fuzzy}$ is then the width of the fuzzy interface; strictly, the
+standard deviation from the average thickness of the interface.
 
-From the reference:
+From Reference [1]:
 
-  The "fuzziness" of the interface is defined by the parameter
-  $\sigma_\text{fuzzy}$. The particle radius $R$ represents the radius of the
-  particle where the scattering length density profile decreased to 1/2 of the
-  core density. $\sigma_\text{fuzzy}$ is the width of the smeared particle
-  surface; i.e., the standard deviation from the average height of the fuzzy
-  interface. The inner regions of the microgel that display a higher density
-  are described by the radial box profile extending to a radius of
-  approximately $R_\text{box} \sim R - 2 \sigma$. The profile approaches
-  zero as $R_\text{sans} \sim R + 2\sigma$.
+  The inner regions ... that display a higher ... [SLD] are described by
+  a radial box profile extending to a radius of approximately $R_\text{box} \sim R - 2 \sigma_\text{fuzzy}$.
+  The profile approaches zero at $R_\text{SANS} \sim R + 2 \sigma_\text{fuzzy}$. Therefore,
+  the overall size of the fuzzy sphere is approximated by ... $R_\text{SANS}$.
+
+For this model to give meaningful results it is important $\sigma_\text{fuzzy} \ll R$.
+It is for the User to ensure that this condition is maintained, especially if
+applyingnpolydispersity to one or both length scales.
+    
+This model has been widely applied to the scattering from polymer microgel
+particles as illustrated below, where $R_\text{h}$ is the hydrodynamic radius.
+
+.. figure:: img/fuzzy_sphere_geometry.jpg
+
+    fuzzy_sphere model applied to a microgel particle (adapted [2], Fig 5).
+
+Although the fuzzy sphere model often provides a good description of scattering
+data from such systems, advances in measurement techniques have highlighted
+that the $real-space$ density profile can be far more complex than this model
+assumes [3].
+
+This model is not suitable for describing spherical particles decorated with
+so-called polymer 'brushes' (where the SLD profile follows a parabolic decay) or
+spherical particles with terminally-attached polymer chains (where the SLD
+profile is expected to exhibit a maximum before the Gaussian decay).
+
+To model more complex SLD profiles, see the onion and spherical_sld models.
 
 For 2D data: The 2D scattering intensity is calculated in the same way as 1D,
 where the $q$ vector is defined as
@@ -53,11 +75,17 @@ References
 #. M Stieger, J. S Pedersen, P Lindner, W Richtering,
    *Langmuir*, 20 (2004) 7283-7292
 
+#. E Ponomareva, B Tadgell, M Hildebrandt, M Krüsmann, S Prévost, P Mulvaney, M Karg,
+   *Soft Matter*, 18 (2022) 807-825
+   
+#. F Scheffold,
+   *Soft Matter*, 20 (2024) 8181-8184
+
 Authorship and Verification
 ----------------------------
 
 * **Author:**
-* **Last Modified by:**
+* **Last Modified by: Steve King Date: April 15, 2026**
 * **Last Reviewed by:**
 """
 
@@ -65,17 +93,17 @@ import numpy as np
 from numpy import inf
 
 name = "fuzzy_sphere"
-title = "Scattering from spherical particles with a fuzzy surface."
+title = "Scattering from spheres with a Gaussian interface."
 description = """\
 scale: scale factor times volume fraction,
 or just volume fraction for absolute scale data
-radius: radius of the solid sphere
-fuzziness = the standard deviation of the fuzzy interfacial
-thickness (ie., so-called interfacial roughness)
+radius: pseudo-radius of the sphere (read the model help)
+fuzziness = standard deviation of the fuzzy interfacial
+thickness
 sld: the SLD of the sphere
-solvend_sld: the SLD of the solvent
-background: incoherent background
-Note: By definition, this function works only when fuzziness << radius.
+sld_solvent: the SLD of the solvent
+background: background
+Note: By definition, this model only works when fuzziness << radius.
 """
 category = "shape:sphere"
 
@@ -83,8 +111,8 @@ category = "shape:sphere"
 # ["name", "units", default, [lower, upper], "type","description"],
 parameters = [["sld",         "1e-6/Ang^2",  1, [-inf, inf], "sld",    "Particle scattering length density"],
               ["sld_solvent", "1e-6/Ang^2",  3, [-inf, inf], "sld",    "Solvent scattering length density"],
-              ["radius",      "Ang",        60, [0, inf],    "volume", "Sphere radius"],
-              ["fuzziness",   "Ang",        10, [0, inf],    "volume",       "std deviation of Gaussian convolution for interface (must be << radius)"],
+              ["radius",      "Ang",        60, [0, inf],    "volume", "Particle pseudo-radius (read the model help)"],
+              ["fuzziness",   "Ang",        10, [0, inf],    "volume",       "Std deviation from average thickness of the fuzzy interface (must be << radius)"],
              ]
 # pylint: enable=bad-whitespace,line-too-long
 
