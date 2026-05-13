c3df432f74d08e77c52d9f0a14e7de383014dccc
Paul Kienzle <pkienzle@nist.gov>
2026-05-13 17:09:51 -0400

    limit max evales for nested adaptive integration: triaxial_ellipsoid

--- sasmodels/models/triaxial_ellipsoid.c
+++ sasmodels/models/triaxial_ellipsoid.c
@@ -94,41 +94,54 @@ Fq(double q,
     double *F2,
     double sld,
     double sld_solvent,
-    double radius_equat_minor,
-    double radius_equat_major,
-    double radius_polar)
+    double Ra,
+    double Rb,
+    double Rc)
 {
+    // Because we are integrating over the entire surface, the orientation is arbitrary. Sort
+    // to Rc > (Ra, Rb) so that the integration will be more accurate.
+    const double maybe_min = fmin(Ra, Rb);
+    const double maybe_max = fmax(Ra, Rb);
+    const double maybe_mid = fmax(maybe_min, Rc);
+    const double radius_equat_minor = fmin(maybe_min, Rc);
+    const double radius_equat_major = fmin(maybe_max, maybe_mid);
+    const double radius_polar = fmax(maybe_max, maybe_mid);
+
     const double pa = square(radius_equat_minor/radius_equat_major) - 1.0;
     const double pc = square(radius_polar/radius_equat_major) - 1.0;
 
-    const double qr_max = q*fmax(fmax(radius_equat_minor, radius_equat_major), radius_polar);
-    constant double *z, *w;
-    int n = gauss_weights(qr_max, 1, &w, &z);
+    const double qr_max_inner = fmax(q*radius_equat_minor, q*radius_equat_major);
+    const double qr_max = fmax(qr_max_inner, q*radius_polar);
+    constant double *z_outer, *w_outer;
+    int n_outer = gauss_weights(qr_max, 1, &w_outer, &z_outer);
+
+    constant double *z_inner, *w_inner;
+    int n_inner = gauss_weights(qr_max_inner, n_outer, &w_inner, &z_inner);
 
     // translate a point in [-1,1] to a point in [0, pi/2]
     const double zm = M_PI_4;
     const double zb = M_PI_4;
     double outer_sum_F1 = 0.0;
     double outer_sum_F2 = 0.0;
-    for (int i=0;i<n;i++) {
+    for (int i=0;i<n_outer;i++) {
         //const double u = z[i]*(upper-lower)/2 + (upper + lower)/2;
-        const double phi = z[i]*zm + zb;
+        const double phi = z_outer[i]*zm + zb;
         const double pa_sinsq_phi = pa*square(sin(phi));
 
         double inner_sum_F1 = 0.0;
         double inner_sum_F2 = 0.0;
         const double um = 0.5;
         const double ub = 0.5;
-        for (int j=0;j<n;j++) {
+        for (int j=0;j<n_inner;j++) {
             // translate a point in [-1,1] to a point in [0, 1]
-            const double usq = square(z[j]*um + ub);
+            const double usq = square(z_inner[j]*um + ub);
             const double r = radius_equat_major*sqrt(pa_sinsq_phi*(1.0-usq) + 1.0 + pc*usq);
             const double fq = sas_3j1x_x(q*r);
-            inner_sum_F1 += w[j] * fq;
-            inner_sum_F2 += w[j] * fq * fq;
+            inner_sum_F1 += w_inner[j] * fq;
+            inner_sum_F2 += w_inner[j] * fq * fq;
         }
-        outer_sum_F1 += w[i] * inner_sum_F1;  // correcting for dx later
-        outer_sum_F2 += w[i] * inner_sum_F2;  // correcting for dx later
+        outer_sum_F1 += w_outer[i] * inner_sum_F1;  // correcting for dx later
+        outer_sum_F2 += w_outer[i] * inner_sum_F2;  // correcting for dx later
     }
     // translate integration ranges from [-1,1] to [lower,upper] and normalize by 4 pi
     outer_sum_F1 *= 0.25;  // = outer*um*zm*8.0/(4.0*M_PI);
